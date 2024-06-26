9a01b16beee86f837004ce5248c086efea908264
Qian Sun <69973222+Chin-Sun@users.noreply.github.com>
2024-06-25 22:57:01 -0400

    Update code.go

--- course/7-loops/exercises/4-loops_fizzbuzz/code.go
+++ course/7-loops/exercises/4-loops_fizzbuzz/code.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
 func fizzbuzz() {
-	// ?
+	for i := 1; i <= 100; i++{
+		if i % 3 && i % 5{
+			fmt.Println("fizzbuzz")
+		}else if i % 3{
+			fmt.Println("fizz")
+		}else if i % 5{
+			fmt.Println("buzz")
+		}
+	}
 }
 
 // don't touch below this line
